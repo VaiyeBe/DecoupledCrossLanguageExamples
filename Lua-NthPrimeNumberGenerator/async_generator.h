d3ecf9d636e97006308d837a7020f989bc2e7544
David <david@templatious.org>
2015-07-26 13:08:24 +0300

    should be good

--- Lua-NthPrimeNumberGenerator/async_generator.h
+++ Lua-NthPrimeNumberGenerator/async_generator.h
@@ -5,8 +5,24 @@
 
 struct Messageable;
 
+#define DUMMY_STRUCT(name) \
+    struct name { template <class T> name(T&&) {} };
+
 struct AsyncPrimeGenerator {
 
+    // signature:
+    // < AsyncJob, int (number), int (update interval, milliseconds) >
+    DUMMY_STRUCT(AsyncJob);
+
+    // signature:
+    // < AsyncUpdate, int (progress) >
+    DUMMY_STRUCT(AsyncUpdate);
+
+    // signature:
+    // < AsyncFinish, int (the number) >
+    DUMMY_STRUCT(AsyncFinish);
+
+    static std::shared_ptr< Messageable > makeGenerator();
 };
 
 #endif /* end of include guard: ASYNC_GENERATOR_HGCV21D5 */
