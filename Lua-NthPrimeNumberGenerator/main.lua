e97e95014635542271df7325148ea994bcf62569
David <david@templatious.org>
2015-07-26 09:39:01 +0300

    processor

--- Lua-NthPrimeNumberGenerator/main.lua
+++ Lua-NthPrimeNumberGenerator/main.lua
@@ -2,3 +2,12 @@ print('hello werld!')
 
 theContext = luaContext()
 mainWnd = theContext:namedMesseagable("mainWnd")
+
+theContext:message(mainWnd,VSig("mwnd_insetlabel"),"CHOLO")
+
+mainWndPushButtonMatch = VMatchFunctor.create(
+    VMatch(function()
+        print('oh noes!')
+    end,VSig("mwnd_outbtnclicked"))
+)
+
